5296515e2b07b1a658ab61282ddbaeed57538ff7
anthonygraca <anthonygraca@gmail.com>
2018-08-22 13:34:59 -0700

    added view function 'board_topics'

--- boards/views.py
+++ boards/views.py
@@ -6,4 +6,7 @@ def home(request):
   boards = Board.objects.all()
   return render(request, 'home.html', {'boards': boards})
 
+def board_topics(request, pk):
+  board = Board.objects.get(pk=pk)
+  return render(request, 'topics.html', {'board': board})
 # Create your views here.
