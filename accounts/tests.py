d11fdcc0857cb1cf507fac7774fd1ae17bf78920
anthonygraca <anthonygraca@gmail.com>
2018-08-27 11:38:34 -0700

    changed test case to use SignUpForm instead of UserCreationForm

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
 from django.urls import resolve
 from django.test import TestCase
 from .views import signup
+from .forms import SignUpForm
 
 class SignUpTests(TestCase):
   def setUp(self):
@@ -22,7 +22,7 @@ class SignUpTests(TestCase):
 
   def test_contains_form(self):
     form = self.response.context.get('form')
-    self.assertIsInstance(form, UserCreationForm)
+    self.assertIsInstance(form, SignUpForm)
 
 class SuccessfulSignUpTests(TestCase):
   def setUp(self):
