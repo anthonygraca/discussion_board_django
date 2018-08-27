44d58ee886c6425be834317fc0145501198f8d48
anthonygraca <anthonygraca@gmail.com>
2018-08-27 10:10:55 -0700

    added accounts app to django

--- forum/settings.py
+++ forum/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
 
     'boards',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
