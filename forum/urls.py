c53344c463bda1629716ae302dfac6c119ef7241
anthonygraca <anthonygraca@gmail.com>
2018-08-28 19:09:07 -0700

    corrected import path

--- forum/urls.py
+++ forum/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url 
-from django.contrib.auth import view as auth_views
+from django.contrib.auth import views as auth_views
 
 from boards import views
 from accounts import views as accounts_views
