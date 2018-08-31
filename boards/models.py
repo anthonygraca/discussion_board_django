a1ee6fd4a1687fe6d0c4785418a1207ac1402971
anthonygraca <anthonygraca@gmail.com>
2018-08-31 09:40:35 -0700

    make board description not required

--- boards/models.py
+++ boards/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Board(models.Model):
   name = models.CharField(max_length=30, unique=True)
-  description = models.CharField(max_length=100)
+  description = models.CharField(max_length=100, blank=True)
 
   def __str__(self):
     return self.name
