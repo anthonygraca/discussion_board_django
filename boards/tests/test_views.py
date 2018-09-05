26c5225f568ddf632a7e4e0598c45db17d48c83f
anthonygraca <anthonygraca@gmail.com>
2018-09-04 18:50:19 -0700

    adding test for @login_required decoration

--- boards/tests/test_views.py
+++ boards/tests/test_views.py
@@ -124,3 +124,12 @@ class NewTopicTests(TestCase):
     form = response.context.get('form')
     self.assertIsInstance(form, NewTopicForm)
 
+class LoginRequiredNewTopicTests(TestCase):
+  def setUp(self):
+    Board.objects.create(name='Django', description='Django board.')
+    self.url = reverse('new_topic', kwargs={'pk': 1})
+    self.response = self.client.get(self.url)
+
+  def test_redirection(self):
+    login_url = reverse('login')
+    self.assertRedirects(self.response, '{login_url}?next=(url)'.format(login_url=login_url, url=self.url))
