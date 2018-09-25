d9fd181b1d19f3d393ced42681a543014142cc9c
anthonygraca <anthonygraca@gmail.com>
2018-09-25 15:17:32 -0700

    added a new view with form

--- boards/views.py
+++ boards/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Board, Topic, Post
-from .forms import NewTopicForm
+from .forms import NewTopicForm, PostForm
 
 def home(request):
   boards = Board.objects.all()
@@ -38,3 +38,17 @@ def topic_posts(request, pk, topic_pk):
   topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
   return render(request, 'topic_posts.html', {'topic': topic })
 
+@login_required
+def reply_topic(request, pk, topic_pk):
+  topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+  if request.method == 'POST':
+    form = PostForm(request.POST)
+    if form.is_valid():
+      post = form.save(commit=False)
+      post.topic = topic
+      post.created_by = request.user
+      post.save()
+      return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+  else:
+    form = PostForm()
+  return render(request, 'reply_topic.html', {'topic': topic, 'form': form}
