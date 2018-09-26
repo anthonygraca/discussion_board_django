b546bd623169e1498dfcdc0f46e9063f0a6b080c
anthonygraca <anthonygraca@gmail.com>
2018-09-26 15:52:36 -0700

    added replies count to topics

--- boards/views.py
+++ boards/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
+from django.db.models import Count
 
 def home(request):
   boards = Board.objects.all()
@@ -11,7 +12,8 @@ def home(request):
 
 def board_topics(request, pk):
   board = get_object_or_404(Board, pk=pk)
-  return render(request, 'topics.html', {'board': board})
+  topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+  return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 @login_required
 def new_topic(request, pk):
