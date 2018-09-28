38c4d91a9b6e2280e567e1bd218acd71a5538e00
anthonygraca <anthonygraca@gmail.com>
2018-09-27 18:21:53 -0700

    override UpdateView

--- boards/views.py
+++ boards/views.py
@@ -58,3 +58,17 @@ def reply_topic(request, pk, topic_pk):
   else:
     form = PostForm()
   return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+class PostUpdateView(UpdateView):
+  model = Post
+  fields = ('message', )
+  template_name= 'edit_post.html'
+  pk_url_kwarg = 'post_pk'
+  context_object_name = 'post'
+
+  def form_valid(self, form):
+    post = form.save(commit=False)
+    post.updated_by = self.request.user
+    post.updated_at = timezone.now()
+    post.save()
+    return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
