5a993042d292a58d19805dd2e179540e3db6b687
anthonygraca <anthonygraca@gmail.com>
2018-10-15 18:09:39 -0700

    applied GCBV paginaation

--- forum/urls.py
+++ forum/urls.py
@@ -55,6 +55,6 @@ urlpatterns = [
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
-    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
     url(r'^admin/', admin.site.urls),
 ]
